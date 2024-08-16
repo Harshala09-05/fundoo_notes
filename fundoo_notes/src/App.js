c9d7044ff4607fd9c591ced2600ee99dd77f87c3
Administrator <pharshala03@gmail.com>
2024-08-16 17:56:30 +0530

    [Harshala]:Register Page

---
+++ fundoo_notes/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Pages/Register';
+// import Login from './Pages/Login';
+// import Router from './Routes/Router';
+
+function App() {
+  // const name = "Hello"
+
+  // const childToParent = (data) => {
+  //   console.log("Child data", data)
+  
+  return (
+  
+    <div className="App">
+      <Register ></Register>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ fundoo_notes/src/Routes/Router.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import Login from '../Pages/Login'
+// import Register from '../Pages/Register'
+// import { Routes } from 'react-router-dom';
+// export default function Router(){
+//   return (
+//     <BrowserRouter>
+//         <Routes>
+//             <Route exact path='/' element={<Login/>}/>
+//             <Route path='/signup' element={<Register/>}/>
+//             </Routes>
+//     </BrowserRouter>
+//   )
+// }
